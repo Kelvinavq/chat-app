db5a795ded0161ff4d4efefb680489151538d2eb
kelvinavq <kvalera200244@gmail.com>
2024-06-01 09:32:23 -0400

    db

--- Server/Config/database.js
+++ Server/Config/database.js
@@ -1,36 +1,30 @@
+// Importar el módulo mysql
 const mysql = require('mysql');
 
-const dbConfig = {
-    host: 'srv960.hstgr.io',
-    user: 'u211881118_chat_app',
-    password: 'Vibradigital2023.',
-    database: 'u211881118_chat_app'
-};
+// Configurar la conexión a la base de datos
+// const db = mysql.createConnection({
+//     host: 'localhost',          
+//     user: 'root',              
+//     password: '',              
+//     database: 'chat-app'        
+// });
 
-let connection;
 
-function handleDisconnect() {
-    connection = mysql.createConnection(dbConfig);
+const db = mysql.createConnection({
+    host: 'srv960.hstgr.io',          
+    user: 'u211881118_chat_app',              
+    password: 'Vibradigital2023.',              
+    database: 'u211881118_chat_app'        
+});
 
-    connection.connect(err => {
-        if (err) {
-            console.error('Error al conectar a la base de datos:', err);
-            setTimeout(handleDisconnect, 2000); // Intentar reconectar después de 2 segundos
-        } else {
-            console.log('Conexión exitosa a la base de datos');
-        }
-    });
+// Conectar a la base de datos
+db.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err);
+    } else {
+        console.log('Conexión exitosa a la base de datos');
+    }
+});
 
-    connection.on('error', err => {
-        console.error('Error en la conexión a la base de datos:', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET') {
-            handleDisconnect(); // Reconectar en caso de pérdida de conexión
-        } else {
-            throw err;
-        }
-    });
-}
-
-handleDisconnect();
-
-module.exports = connection;
+// Exportar la conexión para poder utilizarla en otros archivos
+module.exports = db;
