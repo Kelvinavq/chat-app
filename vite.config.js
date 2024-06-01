507c3006fe643af7ce92de3bf01607e53dd759ad
kelvinavq <kvalera200244@gmail.com>
2024-06-01 10:20:54 -0400

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: './src/Components/Client/Bubble/Bubble_Chat.jsx',
-      name: 'BubbleChatWidget',
-      fileName: (format) => `bubble-chat-widget.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
+  
 });
