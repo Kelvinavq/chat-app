0e5be3482435b93e28d43f91d3af1e8ef00f79fc
kelvinavq <kvalera200244@gmail.com>
2024-06-01 09:11:23 -0400

    build

--- src/Config/Config.jsx
+++ src/Config/Config.jsx
@@ -1,7 +1,7 @@
 const Config = {
-    server_api: "http://localhost:4000/",
+    // server_api: "http://localhost:4000/",
     // server_api: "https://digitalvibra.com/chat/Server/",
-    // server_api: "https://server-4a3z.onrender.com/",
+    server_api: "https://chat-app-28pv.onrender.com/",
   
   };
   
