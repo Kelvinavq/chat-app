7038dd2f96bca053fa2347f448c5dbf004a88e8e
kelvinavq <kvalera200244@gmail.com>
2024-06-01 09:54:43 -0400

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Esta línea es importante, asegúrate de que apunte a la carpeta correcta
+    lib: {
+      entry: './src/Components/Client/Bubble/Bubble_Chat.jsx',
+      name: 'BubbleChatWidget',
+      fileName: (format) => `bubble-chat-widget.${format}.js`,
+    },
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'src/main.jsx'), // Ajusta esta ruta según la ubicación de tu punto de entrada principal
-        // Otros puntos de entrada si los tienes
-      },
+      external: ['react', 'react-dom'],
       output: {
-        entryFileNames: '[name].[hash].js', // Cambia el nombre del archivo de salida si es necesario
-        chunkFileNames: '[name].[hash].js', // Cambia el nombre del archivo de fragmento si es necesario
-        assetFileNames: '[name].[hash].[ext]', // Cambia el nombre del archivo de activo si es necesario
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
       },
     },
   },
