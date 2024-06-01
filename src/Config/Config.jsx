f5d59189815917242c51df7b9681a699c0b23f5c
kelvinavq <kvalera200244@gmail.com>
2024-06-01 19:56:29 -0400

    wbe

--- src/Config/Config.jsx
+++ src/Config/Config.jsx
@@ -1,5 +1,5 @@
 const Config = {
-    server_api: "https://chat-app-28pv.onrender.com:10000",
+    server_api: "https://chat-app-28pv.onrender.com:10000/",
     // server_api: "https://digitalvibra.com/chat/Server/",
     // server_api: "https://chat-app-28pv.onrender.com/",
   
